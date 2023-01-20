afade7e2ebaf46b5cea282a90edf04a15ee69053
Attaullah Syed <44337085+syeda-git@users.noreply.github.com>
2023-01-20 07:48:19 +0530

    test

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,21 @@
+var keywords=[{w:"Overview",p:["p0"]},{w:"of",p:["p0"]},{w:"HCL",p:["p0"]},{w:"ZAO",p:["p0"]},{w:"V2.1.1",p:["p0"]},{w:"RPHE0115W",p:["p1"]},{w:"messages",p:["p2"]},{w:"Syntax",p:["p3","p5"]},{w:"diagrams",p:["p3","p5"]},{w:"reading",p:["p4"]},{w:"How",p:["p5"]},{w:"to",p:["p5"]},{w:"read",p:["p5"]},{w:"the",p:["p5","p13","p14"]},{w:"Oil",p:["p6","p13"]},{w:"Paint",p:["p7"]},{w:"Release",p:["p8"]},{w:"notes",p:["p8"]},{w:"for",p:["p8","p10"]},{w:"Water",p:["p9"]},{w:"hose",p:["p9"]},{w:"IBM",p:["p10"]},{w:"Fault",p:["p10"]},{w:"Analyzer",p:["p10"]},{w:"z/OS",p:["p10"]},{w:"V14.10",p:["p10"]},{w:"documentation",p:["p10"]},{w:"Windshield",p:["p11"]},{w:"washer",p:["p11"]},{w:"fluid",p:["p11"]},{w:"Context",p:["p12"]},{w:"Sensitive",p:["p12"]},{w:"Help",p:["p12"]},{w:"Changing",p:["p13"]},{w:"in",p:["p13"]},{w:"your",p:["p13"]},{w:"car",p:["p13","p14"]},{w:"while",p:["p13"]},{w:"you",p:["p13"]},{w:"are",p:["p13"]},{w:"vacationing",p:["p13"]},{w:"Washing",p:["p14"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5];
+ph["p2"]=[6];
+ph["p3"]=[7, 8];
+ph["p4"]=[9];
+ph["p5"]=[10, 11, 12, 13, 7, 8];
+ph["p6"]=[14];
+ph["p7"]=[15];
+ph["p8"]=[16, 17, 18];
+ph["p9"]=[19, 20];
+ph["p10"]=[21, 22, 23, 18, 24, 25, 26];
+ph["p12"]=[30, 31, 32];
+ph["p11"]=[27, 28, 29];
+ph["p14"]=[41, 13, 36];
+ph["p13"]=[33, 13, 14, 34, 35, 36, 37, 38, 39, 40];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
