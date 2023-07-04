591c4a5e28daad596c5dc9cc19415045b40e526f
Attaullah Syed <attaullah.s@hcl.com>
2023-07-04 08:27:46 +0530

    test

---
+++ webhelp-responsive/v7/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,27 @@
+var keywords=[{w:"Garage",p:["p0","p15"]},{w:"Concepts",p:["p0"]},{w:"Lawnmower",p:["p1"]},{w:"Oil",p:["p2","p14"]},{w:"Paint",p:["p3"]},{w:"Shelving",p:["p4"]},{w:"Snow",p:["p5","p17"]},{w:"shovel",p:["p5"]},{w:"Tool",p:["p6"]},{w:"box",p:["p6"]},{w:"Tools",p:["p7","p16"]},{w:"Water",p:["p8"]},{w:"hose",p:["p8"]},{w:"Wheel",p:["p9"]},{w:"barrow",p:["p9"]},{w:"Workbench",p:["p10","p16"]},{w:"Windshield",p:["p11"]},{w:"washer",p:["p11"]},{w:"fluid",p:["p11"]},{w:"Context",p:["p12"]},{w:"Sensitive",p:["p12"]},{w:"Help",p:["p12"]},{w:"Car",p:["p13","p14","p20"]},{w:"Maintenance",p:["p13"]},{w:"Changing",p:["p14"]},{w:"the",p:["p14","p16","p19","p20"]},{w:"in",p:["p14"]},{w:"your",p:["p14"]},{w:"Tasks",p:["p15"]},{w:"Organizing",p:["p16"]},{w:"and",p:["p16"]},{w:"Shovelling",p:["p17"]},{w:"Spray",p:["p18"]},{w:"painting",p:["p18"]},{w:"Taking",p:["p19"]},{w:"out",p:["p19"]},{w:"garbage",p:["p19"]},{w:"Washing",p:["p20"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2];
+ph["p2"]=[3];
+ph["p3"]=[4];
+ph["p4"]=[5];
+ph["p5"]=[6, 7];
+ph["p6"]=[8, 9];
+ph["p7"]=[10];
+ph["p8"]=[11, 12];
+ph["p9"]=[13, 14];
+ph["p10"]=[15];
+ph["p20"]=[37, 25, 22];
+ph["p12"]=[19, 20, 21];
+ph["p11"]=[16, 17, 18];
+ph["p14"]=[24, 25, 3, 26, 27, 22];
+ph["p13"]=[22, 23];
+ph["p16"]=[29, 25, 15, 30, 10];
+ph["p15"]=[0, 28];
+ph["p18"]=[32, 33];
+ph["p17"]=[31, 6];
+ph["p19"]=[34, 35, 25, 36];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ webhelp-responsive/v7/oxygen-webhelp/template/resources/js/rating.js
@@ -0,0 +1,123 @@
+/*
+ ********************************************************************
+ * Licensed Materials - Property of HCL                             *
+ *                                                                  *
+ * Copyright HCL Software 2020. All Rights Reserved.                *
+ *                                                                  *
+ * Note to US Government Users Restricted Rights:                   *
+ *                                                                  *
+ * Use, duplication or disclosure restricted by GSA ADP Schedule    *
+ *                                                                  *
+ * Author: Emmanuel Palogan                                         *
+ * Release Version: v.Q12023                                         *
+ ********************************************************************
+ */
+import { getIndexLocation, msg } from './utils.js';
+import { LNG_IDX } from './config.js'
+import { LANG_LIST } from './constants.js'
+
+const defaultStarRate = 999;
+const defaultCommentRate = "no comment - ";
+
+export function submitRating() {
+    let userFeedback = document.getElementById('rate-comment').value;
+    let userRate = getStarRating();
+    let selectedLang = getIndexLocation(LNG_IDX)
+    if (LANG_LIST[selectedLang] === undefined) selectedLang = "en"
+//User has to enter feedback in comment box, if he/she has selected both checkbox and star rating  
+    let rateCheck = document.getElementById("rateCheck");
+    if(userRate !== "" && rateCheck.checked !=false && userFeedback == ""){
+
+       alert(msg(selectedLang, "feedback.comment.alert"));
+    } else if (userRate !== "" || userFeedback !== "") {
+//Alerts user to remove personal information from user feedback
+        const emailRegEx = new RegExp(/\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+/, "gm");
+        let contact_num = /([0-9-+:]{7,32})/gm;
+		let IPaddressRegEx = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/gm;
+        if(emailRegEx.test(userFeedback) || contact_num.test(userFeedback) || IPaddressRegEx.test(userFeedback)){
+            alert(msg(selectedLang, "feedback.comment.PII.messagealert"));
+         } else{
+        if (userRate === "") userRate = defaultStarRate;
+        if (userFeedback === "") userFeedback += defaultCommentRate + Math.random().toString(36).substr(2, 9);
+        gtag('event', 'User Feedback', {'event_category': 'rating', 'event_label':userFeedback, 'value':userRate});
+        console.log("Star rating: " + userRate + "\nComment: " + userFeedback);
+        alert(msg(selectedLang, "rating.success.alert"));
+        clearRating();
+        location.reload();
+        }
+
+    } else {
+        alert(msg(selectedLang, "rating.error.alert"));
+    }
+}
+
+export function toggleRateCheckbox() {
+    let rateCheck = document.getElementById("rateCheck");
+    let userFeedback = document.getElementById('rate-comment');
+    userFeedback.value = "";
+    userFeedback.disabled = !rateCheck.checked;
+    let starRating = getStarRating();
+
+//This code enables or disables a submit button based on the checkbox is ticked or not.
+    
+    let submitBtn = document.getElementById('rate-submit');
+
+   if(rateCheck.checked != true) {
+       
+       submitBtn.setAttribute("disabled", true)
+       userFeedback.setAttribute("disabled", true)
+   } else {
+        
+       submitBtn.removeAttribute("disabled")
+       userFeedback.removeAttribute("disabled")
+   }
+    //return rateCheck;
+    if (starRating !== ""){
+        submitBtn.removeAttribute("disabled")
+    }
+}
+
+export function toggleSubmitButton() {
+    let userFeedback = document.getElementById('rate-comment');
+    let starRating = getStarRating();
+    let submitBtn = document.getElementById('rate-submit');
+    
+    if (starRating !== "" || userFeedback !== "") {
+   
+        submitBtn.removeAttribute("disabled")
+        
+    } else {
+        submitBtn.setAttribute("disabled", true)
+    }
+
+}
+
+function clearRating() {
+    let userFeedback = document.getElementById('rate-comment');
+    let starRating = document.getElementsByName('rate');
+    let rateCheck = document.getElementById("rateCheck");
+
+    rateCheck.checked = false;
+    userFeedback.value = "";
+    userFeedback.disabled = true;
+
+    starRating.forEach(function(val){ 
+        if (val.checked) {
+            val.checked = false
+        }
+    })
+}
+
+function getStarRating() {
+    let starRating = document.getElementsByName('rate');
+    let userRate = "";
+
+    starRating.forEach(function(val){ 
+        if (val.checked) {
+            userRate = val.value;
+        }
+    })
+
+
+    return userRate;
+}
